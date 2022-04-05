135f7e077bfd33e0bbbba86cd135e9b44b486565
Horhe305 <101187054+Horhe305@users.noreply.github.com>
2022-04-05 11:24:10 +0000

    koniec

--- Main.java
+++ Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner; 
 class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello world!");
+  public static void main(String[] args)
+  {
+    Scanner Obj = new Scanner(System.in); 
+    String s;
+    System.out.println("Wpisz tekst: ");
+    s = Obj.nextLine();
+    char[] tekst = s.toCharArray();
+    char tab[] = new char[100];
+
+    for (int i = 0; i < tekst.length; i++)
+      {
+        if (tekst[i] == 32) tekst[i] = 95;
+        tab[i] = tekst[i];
+        System.out.print(tab[i]);
+      }
   }
 }
